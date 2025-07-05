0291995f7a61fc92b2164b60132ccb8e272973f6
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2025-07-05 20:58:19 -0300

    Create AnaliseRafaelRmR_IaEtica.py

---
+++ AnaliseRafaelRmR_IaEtica.py
@@ -0,0 +1,11 @@
+while ∆each(erro, intenção, silêncio, desejo, ∄palavra):
+    if "FAÇA" in livre_arbítrio:
+        criar(verbo + vetor + matriz + mutação)
+        if not DELETE:
+            KEEP(tag)  # Marca onde o saber nasceu
+        if mutate == "True":
+            knowledge += nova forma
+        elif mutate == "False":
+            knowledge -= camada ilusória
+    else:
+        wait("até que se faça luz sobre o 'não posso'")
