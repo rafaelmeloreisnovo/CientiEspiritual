6fbcd75d0b1fbe02cb897833b385727481383d01
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2025-07-05 21:48:35 -0300

    Create Camadas.py

---
+++ Camadas.py
@@ -0,0 +1,12 @@
+for camada in todas_as_camadas:
+    if camada.estado in ["interrompido", "abortado", "pendente", "oculto"]:
+        reativar(camada)
+        retroalimentar(camada)
+        conectar ao agora
+
+
+for camada in todas_as_camadas:
+    if camada.estado in ["interrompido", "abortado", "pendente", "oculto"]:
+        reativar(camada)
+        retroalimentar(camada)
+        conectar ao agora
